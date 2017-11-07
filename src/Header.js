6f149dc7329425fd1b03a73d7479ea8833a9f915
Tim Chebunin <tim.phey@gmail.com>
2017-11-07 09:27:55 +0300

    Added fonts and searchbar

--- src/Header.js
+++ src/Header.js
@@ -3,32 +3,52 @@ import logo from "./logo.svg";
 import styled from "styled-components";
 
 const Header = styled.header`
+  /* a flex layout context, always one line */
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+
   background-color: black;
   box-shadow: 0px 0.5px 0px rgba(72, 72, 72, 0.3);
-  max-wight: 100%;
-  max-height: 80px;
-  padding: 20px;
-  padding-left: 70px;
+  height: 80px;
+  /*padding: 20px;*/
   color: #383838;
   margin: 0;
 `;
-const PageTitle = styled.h1`
-  display: inline-flex;
-  text-align: left;
-  font-size: 1.5em;
-  color: red;
+const SearchBar = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: #ffffff;
+
+  font-family: Circular_Air;
+  line-height: 24px;
+  font-size: 16px;
+  color: #383838;
+
+  border: 1px solid rgba(72, 72, 72, 0.2);
+  box-sizing: border-box;
+  box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
+  border-radius: 4px;
+
+  width: 392px;
+  height: 48px;
 `;
 const AppLogo = styled.img`
-  display: inline-flex;
   width: 29.77px;
-  top: 24.01px;
+  margin: 50px;
 `;
 
 export default function() {
   return (
     <Header>
       <AppLogo src={logo} />
-      <PageTitle>Hello world!</PageTitle>
+      <SearchBar>
+        <p>Yeah!</p>
+        <p>Cool guys!</p>
+      </SearchBar>
     </Header>
   );
 }
